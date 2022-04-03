369b34829e269534d5061a27cb4a6b4c48a9dd94
And6D <66632474+And6D@users.noreply.github.com>
2022-04-03 18:44:18 +0400

    Коряво, но вытащил

--- hw.py
+++ hw.py
@@ -1,6 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
+import lxml
 
-r = requests.get('https://www.bookvoed.ru/author?id=153717')
-print(r, "статус сервера")
-print(r.headers, "заголовок")
-print(r.text, "текст")
+url = 'https://www.bookvoed.ru/author?id=153717'
+response = requests.get(url) # url можно напрямую записать значение, без присвоения
+soup = BeautifulSoup(response.text, 'lxml')
+print(soup.h2.text)
+
+items = soup.find_all('div', class_='GT')
+print(items)

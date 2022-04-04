ab2fcf1f79dc8fff822dbba53b01b6d9aa85f782
And6D <66632474+And6D@users.noreply.github.com>
2022-04-04 09:01:10 +0400

    Забирает занные по ссылке, формирут список

--- hw.py
+++ hw.py
@@ -5,7 +5,12 @@ import lxml
 url = 'https://www.bookvoed.ru/author?id=153717'
 response = requests.get(url) # url можно напрямую записать значение, без присвоения
 soup = BeautifulSoup(response.text, 'lxml')
-print(soup.h2.text)
 
-items = soup.find_all('div', class_='GT')
-print(items)
+items = soup.find('div', class_='GT')
+
+spisok = []
+for eachPart in items:
+    spisok.append(eachPart.get_text(strip=True))
+spisok = list(filter(None, spisok))
+
+print(spisok)

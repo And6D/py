9c6dad0667a4c3d6917ddaa62d41d4e1abc4c6c0
And6D <66632474+And6D@users.noreply.github.com>
2022-05-18 10:34:44 +0400

    p112 random access upd exdended range

--- Michael Dawson/chapter 4/p112 random_access.py
+++ Michael Dawson/chapter 4/p112 random_access.py
@@ -1,10 +1,11 @@
 # random letters
+# len
 import random
 word = "index"
 print ("В переменной word хранится слово: ", word, "\n")
 high = len(word)
 low = -len(word)
-for i in range(10):
+for i in range(15):
  position = random.randrange(low, high)
- print ( "word[", position, "]\t", word[position])
+ print ("word[", position, "]\t", word[position])
 input("\n\nHaжмитe Enter. чтобы выйти.")

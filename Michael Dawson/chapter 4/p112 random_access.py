a9c31e9ba581b26f8ab4a4eb8df2cf9e178cc2c7
And6D <66632474+And6D@users.noreply.github.com>
2022-05-18 08:57:08 +0400

    p112 random access

---
+++ Michael Dawson/chapter 4/p112 random_access.py
@@ -0,0 +1,10 @@
+# random letters
+import random
+word = "index"
+print ("В переменной word хранится слово: ", word, "\n")
+high = len(word)
+low = -len(word)
+for i in range(10):
+ position = random.randrange(low, high)
+ print ( "word[", position, "]\t", word[position])
+input("\n\nHaжмитe Enter. чтобы выйти.")

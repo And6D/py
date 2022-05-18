abd55e2cefe3f5b522b69670b4e5978c0703782e
And6D <66632474+And6D@users.noreply.github.com>
2022-05-18 20:57:33 +0400

    инвентарь 2.0

---
+++ Michael Dawson/chapter 4/p126 hero's inventory_2.0.py
@@ -0,0 +1,22 @@
+#Арсенал героя 2.0
+#Демонстрирует работу с кортежами
+#создадим кортеж с несколькими элементами и выведем его с помощью цикла for
+inventory = (
+    "sword",
+    "armor",
+    "shield",
+    "healing poison")
+print("\nSo,in your inventory:")
+for item in inventory:
+    print(item)
+# найдем длину кортежа
+print("\nYou have ", len(inventory), "items in inventory")
+# проверка на принадлежность кортежу с помощью in
+if "healing poison" in inventory:
+    print("You still can fight")
+# отобразим срез
+start = int(input("\nEnter start index of cut: "))
+finish = int(input("Enter final index of cut: "))
+print("Cut inventory[", start, ":", finish, "] - this", end=" ")
+print(inventory[start:finish])
+input("\nHaжмитe Enter. чтобы продолжить.")

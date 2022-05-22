f162e7e63b963b77d1e71471c7090a7ff31060ad
And6D <66632474+And6D@users.noreply.github.com>
2022-05-22 17:27:23 +0400

    task 1  fake agree gialog

--- Michael Dawson/chapter 4/p133 task_1 agile_counter.py
+++ Michael Dawson/chapter 4/p133 task_1 agile_counter.py
@@ -2,20 +2,27 @@
 # input set start and finish numbers by user
 # set step of count by user
 #  в цикл?
-print ("Для начала рассчёта введите диапазон" )
-start_num =int(input("Введите первое число диапазона: "))
-end_num =int(input("Для задания диапазона ввведите конечное число: "))
-step =int(input("Задайте значение шага подсчёта: "))
+print ("Enter range, for count " )
+start_num =int(input("It starts at : "))
+end_num =int(input("and end at: "))
+step =int(input("Enter step: "))
 print("для подсчета доступен диапазон: ", start_num, "до", end_num, "с шагом ", step )
-# диалог для запуска цикал или возрата к вводу знасений и шага
-agreement = 1
-disagreement = 2
-restart = 3
+# диалог для запуска циклf или возрата к вводу знасений и шага
 
-
-
-
-
+agreement =input ("\nFor start tape '1',\n for restart tape '2,\n or '3' for exit: ")
 
 for i in range(start_num, end_num, step):
  print(i, end=" ")
+
+
+
+#agree =1
+#disagree = 2
+#exit= 3
+
+
+
+
+
+
+

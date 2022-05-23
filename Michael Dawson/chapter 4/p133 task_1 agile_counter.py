dd1dfba68b4422bfc3675ca0ad799f76a6376e71
And6D <66632474+And6D@users.noreply.github.com>
2022-05-23 21:19:41 +0400

    task 1 work

--- Michael Dawson/chapter 4/p133 task_1 agile_counter.py
+++ Michael Dawson/chapter 4/p133 task_1 agile_counter.py
@@ -1,25 +1,21 @@
-# by user request count numbers
-# input set start and finish numbers by user
-# set step of count by user
-#  в цикл?
-print ("Enter range, for count\n" )
-
-start_num =int(input("start value :"))
-end_num =int(input("and end value:"))
-step =int(input("enter step:"))
-
-print("count range is:\t", start_num, "-", end_num, "\tstep", step)
-print("\nReady or not?")
 
 agreement_before =int(input("\n1. tape '1' to count"
-                            "\n2. tape '2' for re-enter value"
-                            "\n3. tape '3' for exit"
-                            "\n"))
-print("\nresult: \n")
-for i in range(start_num, end_num, step):
-  print( i, end=" ")
+                            "\n2. tape '2' for exit"))
 
-agreement_after =input("\nCount again  Y/N  ?\n\n")
+while agreement_before == 1:
+
+    print("Enter range, for count\n")
+    start_num = int(input("start value :"))
+    end_num = int(input("and end value:"))
+    step = int(input("enter step:"))
+    print("count range is:\t", start_num, "-", end_num, "\tstep", step)
+    for i in range(start_num, end_num, step):
+     print(i, end=" ")
+    break
+
+if agreement_before == 2:
+    input("\n\nPress Enter to exit.")
+
 
 
 

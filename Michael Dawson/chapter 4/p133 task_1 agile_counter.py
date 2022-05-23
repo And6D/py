5c55ce8556011ef79594fce10a948d9579d94a5b
And6D <66632474+And6D@users.noreply.github.com>
2022-05-23 10:27:07 +0400

    task 1 agree gialog input + restat/exit dialog

--- Michael Dawson/chapter 4/p133 task_1 agile_counter.py
+++ Michael Dawson/chapter 4/p133 task_1 agile_counter.py
@@ -2,29 +2,24 @@
 # input set start and finish numbers by user
 # set step of count by user
 #  в цикл?
-print ("Enter range, for count " )
-start_num =int(input("It starts at : "))
-end_num =int(input("and end at: "))
-step =int(input("Enter step: "))
-print("для подсчета доступен диапазон: ", start_num, "до", end_num, "с шагом ", step )
+print ("Enter range, for count\n" )
 
-range_num=(start_num, end_num)
-print(len(range_num), "count range")
-# диалог для запуска цикл или возрата к вводу значений и шага
-# расчет идет когда start_num >= end_num
+start_num =int(input("start value :"))
+end_num =int(input("and end value:"))
+step =int(input("enter step:"))
 
-agreement =input ("\n1. tape '1' to count:"
-                  "\n2. tape '2' for re-enter value:"
-                  "\n3. tape '3' for exit :")
+print("count range is:\t", start_num, "-", end_num, "\tstep", step)
+print("\nReady or not?")
 
+agreement_before =int(input("\n1. tape '1' to count"
+                            "\n2. tape '2' for re-enter value"
+                            "\n3. tape '3' for exit"
+                            "\n"))
+print("\nresult: \n")
 for i in range(start_num, end_num, step):
- print(i, end=" ")
+  print( i, end=" ")
 
-
-
-#agree =1
-#disagree = 2
-#exit= 3
+agreement_after =input("\nCount again  Y/N  ?\n\n")
 
 
 

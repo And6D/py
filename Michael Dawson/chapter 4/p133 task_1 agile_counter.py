cd9c6717a709bee2d0db86bb0fb3af1e44d781bb
And6D <66632474+And6D@users.noreply.github.com>
2022-05-24 08:55:23 +0400

    task 1 count work

--- Michael Dawson/chapter 4/p133 task_1 agile_counter.py
+++ Michael Dawson/chapter 4/p133 task_1 agile_counter.py
@@ -1,7 +1,9 @@
 
 agreement_before =int(input("\n1. tape '1' to count"
-                            "\n2. tape '2' for exit"))
+                            "\n2. tape '2' for exit\n"))
 
+# включить проверку на пустой ввод
+# обработка ошибок
 while agreement_before == 1:
 
     print("Enter range, for count\n")
@@ -9,9 +11,24 @@ while agreement_before == 1:
     end_num = int(input("and end value:"))
     step = int(input("enter step:"))
     print("count range is:\t", start_num, "-", end_num, "\tstep", step)
+
+    agreement_in = input("\nto continue and count type 'Y' or 'N' to re-enter Values: \n")
+
+
     for i in range(start_num, end_num, step):
-     print(i, end=" ")
+       print(i, end=" ")
     break
+
+    # Включить обработку на пустой ввод
+    count = "Y"
+    restart ="N"
+# любой ввод возвращает в начала цикла
+# нужен  вложеный цикл при Y
+    while agreement_in == count:
+      for i in range(start_num, end_num, step):
+       print(i, end=" ")
+    break
+#  при N в начало цикла
 
 if agreement_before == 2:
     input("\n\nPress Enter to exit.")

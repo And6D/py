8ef629093b347660e665d178b94cf500335487ec
And6D <66632474+And6D@users.noreply.github.com>
2022-05-30 16:46:51 +0400

    rebild

--- Michael Dawson/chapter 4/p133 task_1 agile_counter.py
+++ Michael Dawson/chapter 4/p133 task_1 agile_counter.py
@@ -4,22 +4,25 @@ start_num = int(input("start value :"))
 end_num = int(input("and end value:"))
 step = int(input("enter step:"))
 print("count range is:\t", start_num, "-", end_num, "\tstep", step)
-input("Enter to count")
-for i in range(start_num, end_num, step):
-    i <= end_num
-    print(i, end=" ")
 
-
-
-
-
-
-
-
-
-
-
-
+choise = int(input("\n1.count\n2.set again\n3.exit"))
+calcualte=1
+restart=2
+exit =3
+while choise == restart:
+    start_num = int(input("\nstart value :"))
+    end_num = int(input("and end value:"))
+    step = int(input("enter step:"))
+    print("count range is:\t", start_num, "-", end_num, "\tstep", step)
+    choise = int(input("\n1.count\n2.set again\n3.exit"))
+else:
+    if choise ==calcualte:
+        for i in range(start_num, end_num, step):
+            i <= end_num
+            print(i, end=" ")
+            print()
+    if choise==exit:
+     print("enter to exit")
 
 
 

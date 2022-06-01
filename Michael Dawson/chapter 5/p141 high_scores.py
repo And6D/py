636288258fc2c828541afa55953d15c75a4e2845
And6D <66632474+And6D@users.noreply.github.com>
2022-06-01 20:01:30 +0400

    fixed

--- Michael Dawson/chapter 5/p141 high_scores.py
+++ Michael Dawson/chapter 5/p141 high_scores.py
@@ -17,26 +17,26 @@ while choice != "0":
     choice = input("You choice: ")
     print()
 # exit
-if choice == "0":
-    print("Good byе")
+    if choice == "0":
+     print("Good byе")
 # вывод лучших результатов на экран
-elif choice == "1":
-    print("Highscores")
-    for score in scores:
+    elif choice == "1":
+     print("Highscores")
+     for score in scores:
         print(score)
 # add record
-elif choice == "2":
+    elif choice == "2":
      score = int(input("Write you highscores"))
      scores.append(score)
-elif choice == "3":
-    score = int(input("Which highscores you want to delete?: "))
-    if score in scores:
+    elif choice == "3":
+     score = int(input("Which highscores you want to delete?: "))
+     if score in scores:
         scores.remove(score)
-    else:
+     else:
         print(f"Result {score} not find.")
-elif choice == "4":
-     scores.sort(reverse=True)
+    elif choice == "4":
+      scores.sort(reverse=True)
 # miss clic error
-else:
-    print(f"Sorry,no what position in menu {choice}")
+    else:
+     print(f"Sorry,no what position in menu {choice}")
 input("Press Enter to exit")

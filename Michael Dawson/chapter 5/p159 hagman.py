f39efa9241713c66d1cf402a82f0fe4b95935c50
And6D <66632474+And6D@users.noreply.github.com>
2022-06-06 21:40:10 +0400

    write # till main circle

--- Michael Dawson/chapter 5/p159 hagman.py
+++ Michael Dawson/chapter 5/p159 hagman.py
@@ -1,3 +1,109 @@
 import random
+
 # constant hangman
-HANGMAN = ("""0""")
+HANGMAN = (
+    """
+    -------
+    |     |
+    |
+    |
+    |
+    |
+    |
+    |
+-------------
+""",
+    """
+    -------
+    |     |
+    |     0  |
+    |
+    |
+    |
+    |
+    |
+-------------
+""",
+    """
+    -------
+    |     |
+    |     0  |  -+-
+    |
+    |
+    |
+    |
+    |
+-------------
+""",
+    """
+    -------
+    |     |
+    |     0  |  /-+-
+    |
+    |
+    |
+    |
+    |
+-------------
+""",
+    """
+    -------
+    |     |
+    |     0  |  /-+-/
+    |     
+    |
+    |
+    |
+    |
+-------------
+""",
+    """
+    -------
+    |     |
+    |     0  |  /-+-/
+    |     |
+    |
+    |
+    |
+    |
+-------------
+""",
+    """
+    -------
+    |     |
+    |     0  |  /-+-/
+    |     |
+    |     |
+    |    |
+    |    |
+    |
+-------------
+""",
+    """
+    -------
+    |     |
+    |     0  |  /-+-/
+    |     |
+    |     |
+    |    |  |
+    |    |  |
+    |
+------------
+""")
+# max mistake
+MAX_WRONG = len(HANGMAN) -1
+WORDS = ("OVERUSED", "CLAM", "GUAM", "TAFFETA", "PYTHON")
+# initialise variable
+word = random.choice(WORDS)  # word for gees
+so_far = "-" * len(word)  # one "-" on each symbol, wat need to guess
+wrong = 0  # number of wrong user choices
+used = []  # symbol's what user already typed
+# main circle
+print("Welcome to 'HANGMAN' game, Good like.")
+while wrong < MAX_WRONG and so_far !=word:
+    print(f"{HANGMAN[wrong]}"
+          f"\n You already use this letters:"
+          f"\n {used}"
+          f"\n Now you guess"
+          f"\n {so_far}")
+

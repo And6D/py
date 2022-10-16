a7ffd78b7f38aadb012758a27aca3fe14737cb27
And6D <mushtakov@gmail.com>
2022-10-16 13:11:48 +0400

    fix spaces

--- Michael Dawson/chapter 6/p174(167) birthday_wishes.py
+++ Michael Dawson/chapter 6/p174(167) birthday_wishes.py
@@ -1,7 +1,8 @@
 # Birthday Wishes
 # Demonstrates keyword arguments and default parameter values
-# positional parameters
 
+
+# positional parameters
 def birthday1(name, age):
     print("Happy birthday,", name, "!", " I hear you're", age, "today.\n")
 

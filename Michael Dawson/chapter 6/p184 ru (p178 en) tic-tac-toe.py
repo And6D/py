fa432185220a26dc907431d87f1e2522ed99aeca
And6D <mushtakov@gmail.com>
2022-10-20 12:06:51 +0400

    added function: ask number                 pieces                 new board                 display board

--- Michael Dawson/chapter 6/p184 ru (p178 en) tic-tac-toe.py
+++ Michael Dawson/chapter 6/p184 ru (p178 en) tic-tac-toe.py
@@ -7,10 +7,11 @@ EMPTY = " "
 TIE = "TIE"
 NUM_SQUARES = 9
 
+
 def display_instruct():
- """Display game instructions."""
- print(
- """
+    """Display game instructions."""
+    print(
+        """
  Welcome to the greatest intellectual challenge of all time: Tic-Tac-Toe.
  This will be a showdown between your human brain and my silicon processor.
  You will make your move known by entering a number, 0 - 8. The number
@@ -23,4 +24,51 @@ def display_instruct():
      
  Prepare yourself, human. The ultimate battle is about to begin. \n
  """
- )
+    )
+
+
+def ask_yes_no(question):
+    """Ask a yes or no question."""
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+    return response
+
+
+def ask_number(question, low, high):
+    """Ask for a number within a range."""
+    response = None
+    while response not in range(low, high):
+        response = int(input(question))
+    return response
+
+
+def pieces():
+    """Determine if player or computer goes first."""
+    go_first = ask_yes_no("Do you require the first move? (y/n): ")
+    if go_first == "y":
+        print("\nThen take the first move. You will need it.")
+        human = X
+        computer = O
+    else:
+        print("\nYour bravery will be your undoing... I will go first.")
+        computer = X
+        human = O
+    return computer, human
+
+
+def new_board():
+    """Create new game board."""
+    board = []
+    for square in range(NUM_SQUARES):
+        board.append(EMPTY)
+    return board
+
+
+def display_board(board):
+    """Display game board on screen."""
+    print("\n\t", board[0], "|", board[1], "|", board[2])
+    print("\t", "---------")
+    print("\t", board[3], "|", board[4], "|", board[5])
+    print("\t", "---------")
+    print("\t", board[6], "|", board[7], "|", board[8], "\n")

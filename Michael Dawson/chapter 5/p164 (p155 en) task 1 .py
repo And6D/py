3786fa3ba9c5e704cef07718617eb8f2f4b9b4a5
And6D <66632474+And6D@users.noreply.github.com>
2022-10-27 16:49:40 +0400

    holder

---
+++ Michael Dawson/chapter 5/p164 (p155 en) task 1 .py
@@ -0,0 +1,14 @@
+""" 1. Create a program that prints a list of words in random order.
+    2. The program should print all the words and not repeat any."""
+import random
+
+# dictionary
+WORDS = ["bullet", "sword", "turbojet",
+         "light", "sugar", "sweet",
+         "bicycle", "phone", "song",
+         "face", "t-shirt", "smile"]
+# main
+# print all word from WORDS
+# while if word not duble in WORDS print word
+
+
